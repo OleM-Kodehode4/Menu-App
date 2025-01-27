9cb39d73ca7dfe4947e3076326f46e5ee9f856c7
OleM-Kodehode4 <olembrande@gmail.com>
2025-01-27 12:30:05 +0100

    update homepage URL and adjust Vite base path to match new repository name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-menu-app/',  // Sørg for at dette matcher repo-navnet på GitHub
-})
+  base: "/Menu-App/", // Sørg for at dette matcher repo-navnet på GitHub
+});
